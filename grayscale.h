0ab44d0f0f9b2861a05437119a9ab4987a760b09
lina <freetogo666@outlook.com>
2024-12-05 12:15:48 +0400

    Add project files.

---
+++ grayscale.h
@@ -0,0 +1,355 @@
+#include <iostream>
+#include <random>
+#include <limits>
+#include <cmath>
+
+template <typename T>
+class Grayscale {
+private:
+    size_t _width;
+    size_t _height;
+    T* _data;
+public:
+	static constexpr double epsilon = 1e-6;
+
+	Grayscale<T>::Grayscale(size_t height, size_t width, bool fill_random = true) : _height(height), _width(width) {
+
+		_data = new T[height * width];
+
+		std::random_device rd;
+		std::mt19937 gen(rd());
+
+		if (fill_random) {
+
+			if (std::is_same_v<T, float>) {
+				for (size_t i = 0; i < width * height; ++i) {
+					std::uniform_real_distribution<> dist(0.0, 255.0);
+					_data[i] = static_cast<T>(dist(gen));
+				}
+			}
+			else if (std::is_same_v<T, bool>) {
+				for (size_t i = 0; i < width * height; ++i) {
+					std::uniform_int_distribution<> dist(0, 1);
+					_data[i] = static_cast<T>(dist(gen));
+				}
+			}
+			else if (std::is_same_v<T, short> || std::is_same_v<T, char>) {
+				for (size_t i = 0; i < width * height; ++i) {
+					std::uniform_int_distribution<> dist(0, 255);
+					_data[i] = static_cast<T>(dist(gen));
+				}
+			}
+		}
+		else {
+			for (size_t i = 0; i < width * height; ++i) {
+				_data[i] = 0;
+			}
+		}
+	}
+
+	Grayscale(const Grayscale& other) {
+
+		_height = other._height;
+		_width = other._width;
+		_data = new T[_width * _height];
+
+		for (size_t i = 0; i < _width * _height; ++i) {
+			_data[i] = other._data[i];
+		}
+	}
+
+	Grayscale& operator=(const Grayscale& other) {
+		if (this != &other) {
+			delete[] _data;
+			_height = other._height;
+			_width = other._width;
+			_data = new T[_width * _height];
+
+			for (size_t i = 0; i < _width * _height; ++i) {
+				_data[i] = other._data[i];
+			}
+		}
+		return *this;
+	}
+
+	~Grayscale() {
+		delete[] _data;
+	}
+
+	T& operator()(size_t x, size_t y) {
+		if (x >= _width || y >= _height) {
+			throw std::out_of_range("index out of range");
+		}
+		return _data[y * _width + x];
+	}
+
+	const T& operator()(size_t x, size_t y) const {
+		if (x >= _width || y >= _height) {
+			throw std::out_of_range("index out of range");
+		}
+		return _data[y * _width + x];
+	}
+
+	Grayscale operator*(const Grayscale& other) const {
+		if (_width != other._width || _height != other._height) {
+			throw std::invalid_argument("sizes don't match");
+		}
+
+		T max = std::numeric_limits<T>::max();
+		T min = std::numeric_limits<T>::min();
+
+		Grayscale<T> res(_width, _height);
+
+		for (size_t i = 0; i < _width * _height; ++i) {
+
+			if (_data[i] != 0 && other._data[i] > max / _data[i]) {
+				res._data[i] = max;
+			}
+			else if (_data[i] != 0 && other._data[i] < min / _data[i]) {
+				res._data[i] = min;
+			}
+			else {
+				res._data[i] = _data[i] * other._data[i];
+			}
+		}
+		return res;
+	}
+
+	Grayscale operator*(T scalar) {
+
+		T max = std::numeric_limits<T>::max();
+		T min = std::numeric_limits<T>::min();
+
+		Grayscale<T> res(_width, _height);
+
+		for (size_t i = 0; i < _width * _height; ++i) {
+			if (_data[i] != 0 && scalar > max / _data[i]) {
+				res._data[i] = max;
+			}
+			else if (_data[i] != 0 && scalar < min / _data[i]) {
+				res._data[i] = min;
+			}
+			else {
+				res._data[i] = _data[i] * scalar;
+			}
+		}
+		return res;
+	}
+
+	Grayscale& operator*=(T scalar) {
+
+		T max = std::numeric_limits<T>::max();
+		T min = std::numeric_limits<T>::min();
+
+		for (size_t i = 0; i < _width * _height; ++i) {
+			if (_data[i] != 0 && scalar > max / _data[i]) {
+				_data[i] = max;
+			}
+			else if (_data[i] != 0 && scalar < min / _data[i]) {
+				_data[i] = min;
+			}
+			else {
+				_data[i] *= scalar;
+			}
+		}
+		return *this;
+	}
+
+	Grayscale operator+(const Grayscale& other) const {
+		size_t width = std::max(_width, other._width);
+		size_t height = std::max(_height, other._height);
+
+		T max = std::numeric_limits<T>::max();
+
+		Grayscale<T> res(width, height);
+
+		for (size_t y = 0; y < height; ++y) {
+			for (size_t x = 0; x < width; ++x) {
+				T px1 = (x < _width && y < _height) ? _data[y * _width + x] : 0;
+				T px2 = (x < other._width && y < other._height) ? other._data[y * other._width + x] : 0;
+
+				if (px1 > max - px2) {
+					res(x,y) = max;
+				}
+				else {
+					res(x,y) = px1 + px2;
+				}
+			}
+		}
+		return res;
+	}
+
+	Grayscale operator+(T scalar) {
+		T max = std::numeric_limits<T>::max();
+
+		Grayscale<T> res(_width, _height);
+
+		for (size_t i = 0; i < _width * _height; ++i) {
+			if (_data[i] > max - scalar) {
+				res._data[i] = max;
+			}
+			else {
+				res._data[i] = _data[i] + scalar;
+			}
+		}
+		return res;
+	}
+
+	Grayscale& operator+=(T scalar) {
+		T max = std::numeric_limits<T>::max();
+
+		for (size_t i = 0; i < _width * _height; ++i) {
+			if (_data[i] > max - scalar) {
+				_data[i] = max;
+			}
+			else {
+				_data[i] += scalar;
+			}
+		}
+		return *this;
+	}
+
+	Grayscale operator-(const Grayscale& other) const {
+		size_t width = std::max(_width, other._width);
+		size_t height = std::max(_height, other._height);
+
+		T min = std::numeric_limits<T>::min();
+
+		Grayscale<T> res(width, height);
+
+		for (size_t y = 0; y < height; ++y) {
+			for (size_t x = 0; x < width; ++x) {
+				T px1 = (x < _width && y < _height) ? _data[y * _width + x] : 0;
+				T px2 = (x < other._width && y < other._height) ? other._data[y * other._width + x] : 0;
+
+				if (px1 < min + px2) {
+					res(x, y) = min;
+				}
+				else {
+					res(x, y) = px1 - px2;
+				}
+			}
+		}
+		return res;
+	}
+
+	Grayscale operator-(T scalar) {
+		T min = std::numeric_limits<T>::min();
+
+		Grayscale<T> res(_width, _height);
+
+		for (size_t i = 0; i < _width * _height; ++i) {
+			if (_data[i] < min + scalar) {
+				res._data[i] = min;
+			}
+			else {
+				res._data[i] = _data[i] - scalar;
+			}
+		}
+		return res;
+	}
+
+	Grayscale operator!() const {
+		Grayscale res(_width, _height);
+		for (size_t i = 0; i < _width * _height; ++i) {
+			if (std::is_same_v<T, bool>) {
+				res._data[i] = !_data[i];
+			}
+			else {
+				res._data[i] = _data[i] * (-1);
+			}
+		}
+		return res;
+	}
+
+	bool operator==(const Grayscale& other) const {
+		if (_width != other._width || _height != other._height) {
+			return false;
+		}
+		for (size_t i = 0; i < _width * _height; ++i) {
+			if (std::fabs(_data[i] - other._data[i]) > epsilon) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool operator!=(const Grayscale& other) const {
+		return !(*this == other);
+	}
+
+	double fill_coefficient() const {
+		double sum = 0.0;
+
+		for (size_t i = 0; i < _height * _width; ++i) {
+			sum += static_cast<double>(_data[i]);
+		}
+
+		return sum / (_height * _width * (double)std::numeric_limits<T>::max());
+	}
+
+	size_t get_height() const {
+		return _height;
+	}
+
+	size_t get_width() const {
+		return _width;
+	}
+
+
+	void draw_rectangle(size_t x1, size_t y1, size_t x2, size_t y2, T fill_value) {
+		for (size_t y = std::min(y1, y2); y <= std::max(y1, y2); ++y) {
+			for (size_t x = std::min(x1, x2); x <= std::max(x1, x2); ++x) {
+					(*this)(x, y) = fill_value;
+			}
+		}
+	}
+};
+
+template <typename T>
+Grayscale<T> operator+(const T scalar, const Grayscale<T>& img) {
+	T max = std::numeric_limits<T>::max();
+
+	Grayscale<T> res(img.get_width(), img.get_height());
+
+	for (size_t y = 0; y < img.get_height(); ++y) {
+		for (size_t x = 0; x < img.get_width(); ++x) {
+			if (img(x, y) > max - scalar) {
+				res(x, y) = max;
+			}
+			else {
+				res(x, y) = scalar + img(x, y);
+			}
+		}
+	}
+	return res;
+}
+
+template <typename T>
+Grayscale<T> operator-(const T scalar, const Grayscale<T>& img) {
+	T min = std::numeric_limits<T>::min();
+	Grayscale<T> res(img.get_width(), img.get_height());
+
+	for (size_t y = 0; y < img.get_height(); ++y) {
+		for (size_t x = 0; x < img.get_width(); ++x) {
+			if (img(x, y) < min + scalar) {
+				res(x, y) = min;
+			}
+			else {
+				res(x, y) = scalar - img(x, y);
+			}
+		}
+	}
+	return res;
+}
+
+template <typename T>
+ std::ostream& operator<<(std::ostream& os, const Grayscale<T>& img) {
+	for (size_t y = 0; y < img.get_height(); ++y) {
+		for (size_t x = 0; x < img.get_width(); ++x) {
+			os << img(x, y) << " ";
+		}
+		os << "\n";
+	}
+	return os;
+}
---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include "grayscale.h"
+#include <iostream>
+#include <stdexcept>
+
+int main() {
+	try {
+
+		short scalar = 10;
+
+		Grayscale<short> img1(3, 3, true);
+		Grayscale<short> img2(3, 3, true);
+
+		auto sum_1 = img1 + img2;
+		auto sum_2 = scalar + img1;
+
+		auto diff_1 = img1 - img2;
+		auto diff_2 = scalar - img1;
+
+		auto product_1 = img1 * img2;
+		auto product_2 = img1 * 10;
+
+		std::cout << "image 1\n" << img1;
+		std::cout << "\nimage 2\n" << img2;
+		std::cout << "\nsum of images\n" << sum_1;
+		std::cout << "\nsum of number and image 1\n" << sum_2;
+		std::cout << "\ndifference of images\n" << diff_1;
+		std::cout << "\ndifference of number and image 1\n" << diff_2;
+		std::cout << "\nproduct of images\n" << product_1;
+		std::cout << "\nproduct of number and image 1\n" << product_2;
+		std::cout << "\ninverted image 1\n" << !img1;
+		std::cout << "\nfill coefficient of image 1\n" << img1.fill_coefficient() << '\n';
+
+
+		Grayscale<short> img3(10, 10, false);
+		short fill = 0;
+
+		size_t x1, y1;
+		size_t x2, y2;
+
+		std::cout << "\nenter top left point ";
+		std::cin >> x1 >> y1;
+
+		std::cout << "\nenter bottom right point ";
+		std::cin >> x2 >> y2;
+
+		std::cout << "\nenter number for filling ";
+		std::cin >> fill;
+
+		img3.draw_rectangle(x1, y1, x2, y2, fill);
+
+		std::cout << img3;
+	}
+	catch (const std::invalid_argument& e) {
+		std::cerr << e.what() << "\n";
+	}
+	catch (const std::out_of_range& e) {
+		std::cerr << e.what() << "\n";
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << "\n";
+	}
+
+	return 0;
+}
